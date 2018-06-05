c2eb00d715a7e684c9b0fed5f796d53ab3fa595e
superman0922 <twxlj88@163.com>
2018-06-05 23:13:34 +0800

    Login

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -18,16 +18,19 @@ from django.contrib import admin
 from xujia import views as xj_views
 from xujia.controllers.roast import views as roast_views
 from xujia.controllers.article import views as article_views
+from xujia.controllers.auth import user
 
 urlpatterns = [
     url(r'^register$', xj_views.register, name='register'),
     url(r'^login$', xj_views.login, name='login'),
-    url(r'^logout$', xj_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^roast/index$', roast_views.index, name='roast_index'),
     url(r'^roast/api_talk', roast_views.api_talk, name='api_talk'),
     url(r'^roast/api_get_list', roast_views.api_get_list, name='api_get_list'),
     url(r'^article/index$', article_views.index, name='article_index'),
     url(r'^article/publish', article_views.publish, name='article_publish'),
+    url(r'^auth/user/login', user.login, name='user/login'),
+    url(r'^auth/user/register', user.register, name='user/register'),
+    url(r'^auth/user/logout', user.logout, name='user/logout'),
     url(r'^$', xj_views.index, name='index'),
 ]
